4000ae03ae37bb72a9b6fe06524d3f981d337f19
FractionalEmpire <david@texastax.loan>
2026-05-02 18:00:09 -0400

    [Claude] Remove AppShell wrapper from tasks page (now global in layout)

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,14 +1,9 @@
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AppShell from "@/components/AppShell";
 import TasksClient from "./TasksClient";
 
 export default async function TasksPage() {
   const user = getCurrentUser();
   if (!user) redirect("/login");
-  return (
-    <AppShell user={user}>
-      <TasksClient user={user} />
-    </AppShell>
-  );
+  return <TasksClient user={user} />;
 }

1b1c3462f17694c531a6ecb817d1258db4a98d41
FractionalEmpire <david@texastax.loan>
2026-05-02 18:00:11 -0400

    [Claude] Remove AppShell wrapper from docs page (now global in layout)

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,14 +1,9 @@
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AppShell from "@/components/AppShell";
 import DocsClient from "./DocsClient";
 
 export default async function DocsPage() {
   const user = getCurrentUser();
   if (!user) redirect("/login");
-  return (
-    <AppShell user={user}>
-      <DocsClient user={user} />
-    </AppShell>
-  );
+  return <DocsClient user={user} />;
 }

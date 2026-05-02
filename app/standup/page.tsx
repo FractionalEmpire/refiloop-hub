67be25cbcc146eaa1f4a9e1e95ffeccfb8edc601
FractionalEmpire <david@texastax.loan>
2026-05-02 18:00:10 -0400

    [Claude] Remove AppShell wrapper from standup page (now global in layout)

--- app/standup/page.tsx
+++ app/standup/page.tsx
@@ -1,14 +1,9 @@
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AppShell from "@/components/AppShell";
 import StandupClient from "./StandupClient";
 
 export default async function StandupPage() {
   const user = getCurrentUser();
   if (!user) redirect("/login");
-  return (
-    <AppShell user={user}>
-      <StandupClient user={user} />
-    </AppShell>
-  );
+  return <StandupClient user={user} />;
 }

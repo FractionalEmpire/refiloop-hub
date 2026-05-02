d7153c70532a8cf890fdde9084c2d7fdbeb14d4d
FractionalEmpire <david@texastax.loan>
2026-05-02 18:00:10 -0400

    [Claude] Remove AppShell wrapper from projects page (now global in layout)

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,9 @@
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AppShell from "@/components/AppShell";
 import ProjectsClient from "./ProjectsClient";
 
 export default async function ProjectsPage() {
   const user = getCurrentUser();
   if (!user) redirect("/login");
-  return (
-    <AppShell user={user}>
-      <ProjectsClient />
-    </AppShell>
-  );
+  return <ProjectsClient />;
 }

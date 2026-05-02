3f7971ff099af81d09519bec4bbfffd48c98cac8
FractionalEmpire <david@texastax.loan>
2026-05-02 18:00:12 -0400

    [Claude] Remove AppShell wrapper from EOD page (now global in layout)

--- app/eod/page.tsx
+++ app/eod/page.tsx
@@ -1,14 +1,9 @@
 import { getCurrentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AppShell from "@/components/AppShell";
 import EODClient from "./EODClient";
 
 export default async function EODPage() {
   const user = getCurrentUser();
   if (!user) redirect("/login");
-  return (
-    <AppShell user={user}>
-      <EODClient user={user} />
-    </AppShell>
-  );
+  return <EODClient user={user} />;
 }
